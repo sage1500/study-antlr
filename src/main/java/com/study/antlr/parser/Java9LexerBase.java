7d721e2b51297010ae93f8640ac5b63db7301a31
sage1500 <motosage25@gmail.com>
2023-01-15 17:16:11 +0900

    自動生成ソースをリポジトリから除外

--- src/main/java/com/study/antlr/parser/Java9LexerBase.java
+++ src/main/java/com/study/antlr/parser/Java9LexerBase.java
@@ -32,32 +32,25 @@ package com.study.antlr.parser;
 
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Lexer;
-import org.antlr.v4.runtime.Token;
-import org.antlr.v4.runtime.misc.Interval;
 
-public abstract class Java9LexerBase extends Lexer
-{
-    public Java9LexerBase(CharStream input) {
-	super(input);
-    }
+public abstract class Java9LexerBase extends Lexer {
+	public Java9LexerBase(CharStream input) {
+		super(input);
+	}
 
-    public boolean Check1()
-    {
-        return Character.isJavaIdentifierStart(_input.LA(-1));
-    }
+	public boolean Check1() {
+		return Character.isJavaIdentifierStart(_input.LA(-1));
+	}
 
-    public boolean Check2()
-    {
-        return Character.isJavaIdentifierStart(Character.toCodePoint((char)_input.LA(-2), (char)_input.LA(-1)));
-    }
+	public boolean Check2() {
+		return Character.isJavaIdentifierStart(Character.toCodePoint((char) _input.LA(-2), (char) _input.LA(-1)));
+	}
 
-    public boolean Check3()
-    {
-        return Character.isJavaIdentifierPart(_input.LA(-1));
-    }
+	public boolean Check3() {
+		return Character.isJavaIdentifierPart(_input.LA(-1));
+	}
 
-    public boolean Check4()
-    {
-        return Character.isJavaIdentifierPart(Character.toCodePoint((char)_input.LA(-2), (char)_input.LA(-1)));
-    }
+	public boolean Check4() {
+		return Character.isJavaIdentifierPart(Character.toCodePoint((char) _input.LA(-2), (char) _input.LA(-1)));
+	}
 }
